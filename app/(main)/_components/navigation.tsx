51bce13d7b628416c889631e191a73fab2cc0325
Kotsasmin <kotsasmin@gmail.com>
2024-10-09 12:59:30 +0300

    Made the documents list and fixed a lot of api errors

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -7,15 +7,16 @@ import { ElementRef, useEffect, useRef, useState } from "react";
 
 import { useMediaQuery} from "usehooks-ts";
 import { UserItem } from "./user-item";
-import { useQuery, useMutation } from "convex/react";
+import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { Item } from "./item";
 import { toast } from "sonner";
+import { DocumentList } from "./document-list";
 
 export const Navigation = () => {
     const pathname = usePathname();
     const isMobile = useMediaQuery("(max-width: 768px)");
-    const documents = useQuery(api.documents.get);
+
     const create = useMutation(api.documents.create);
 
 
@@ -146,10 +147,7 @@ className={cn("h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dar
                <Item onClick={handleCreate} label="New page" icon={PlusCircle}/>
             </div>
             <div className="mt-4">
-                {documents?.map((document) => (
-                    <p key={document._id}>
-                        {document.title}</p>
-                ))}
+                <DocumentList />
             </div>
             <div 
             onMouseDown={handleMouseDown}
