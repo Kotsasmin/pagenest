b309b63060ec74b89d4bb2847e38c60e5573585c
Kotsasmin <kotsasmin@gmail.com>
2024-10-12 13:36:38 +0300

    Now the app works.

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,20 +1,23 @@
 "use client";
+
 import Image from "next/image";
-import { useUser } from "@clerk/clerk-react"
+import { useUser } from "@clerk/clerk-react";
 import { Button } from "@/components/ui/button";
 import { PlusCircle } from "lucide-react";
 import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 const DocumentsPage = () => {
     const { user } = useUser();
+    const router = useRouter();
 
     const create = useMutation(api.documents.create);
 
     const onCreate = () => {
-        const promise = create({ title:"Untitled" });
-
+        const promise = create({ title: "Untitled" })
+            .then((documentId) => router.push(`/documents/${documentId}`))
         toast.promise(promise, {
             loading: "Creating a new page...",
             success: "New page created!",
@@ -25,18 +28,19 @@ const DocumentsPage = () => {
     return (
         <div className="min-h-screen h-full flex flex-col items-center justify-center space-y-4">
             <Image
-            src="/empty.svg"
-            height="300"
-            width="300"
-            alt="Empty"/>
+                src="/empty.svg"
+                height="300"
+                width="300"
+                alt="Empty"
+            />
             <h2 className="text-lg font-medium">
-                Wellcome to {user?.firstName}&apos;s PageNest
+                Welcome to {user?.firstName}&apos;s PageNest
             </h2>
             <Button onClick={onCreate}>
-                <PlusCircle className="h-4 w-4 mr-2"/>Create a page
+                <PlusCircle className="h-4 w-4 mr-2" /> Create a page
             </Button>
         </div>
     );
-}
+};
 
 export default DocumentsPage;
